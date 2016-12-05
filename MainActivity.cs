2311b3519bac71852188b037de762377da1399e0
eddydn <eddydn@gmail.com>
2016-12-05 09:05:32 +0700

    First Commit
    First Commit

---
+++ MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace XamarinSlideUpPanel
+{
+    [Activity(Label = "XamarinSlideUpPanel", MainLauncher = true, Icon = "@drawable/icon",Theme ="@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ obj/Debug/android/src/md5fe26bc6e302d174b0bda0f73eef4b72b/MainActivity.java
@@ -0,0 +1,47 @@
+package md5fe26bc6e302d174b0bda0f73eef4b72b;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("XamarinSlideUpPanel.MainActivity, XamarinSlideUpPanel, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("XamarinSlideUpPanel.MainActivity, XamarinSlideUpPanel, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
